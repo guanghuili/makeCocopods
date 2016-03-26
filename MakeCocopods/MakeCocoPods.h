748716316c5acf1ae1923fd31c85c2060407b8ac
jeff <jeff.jing@createus.net>
2016-03-26 11:28:17 +0800

    制作cocopods完成

---
+++ MakeCocopods/MakeCocoPods.h
@@ -0,0 +1,15 @@
+//
+//  MakeCocoPods.h
+//  SmartWPK
+//
+//  Created by sprint on 16/3/26.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MakeCocoPods : NSObject
+
+-(void) print;
+
+@end
---
+++ MakeCocopods/MakeCocoPods.m
@@ -0,0 +1,17 @@
+//
+//  MakeCocoPods.m
+//  SmartWPK
+//
+//  Created by sprint on 16/3/26.
+//
+//
+
+#import "MakeCocoPods.h"
+
+@implementation MakeCocoPods
+
+-(void)print {
+    NSLog(@"MakeCocoPods==print");
+}
+
+@end
